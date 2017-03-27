07288e159ab955d9e6e1e4b79ced6177a71a3ce6
matthewalbrecht <MatthewWilliamAlbrecht@gmail.com>
2017-03-27 13:01:18 -0600

    "users table updated correctly"

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('home', {
+    title: 'HOMEPAGE'
+  })
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -12,12 +12,28 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   let username = req.body.username
+  checkUsername(username).then((data) => {
+    console.log('this is data', data.length);
+    if (data.length || !username) {
+      res.sendStatus(400)
+    } else {
+      insertIntoUsers(username)
+        .then((iData) => {
+          console.log('heehehehheheheh');
+          res.redirect('home')
+        })
+      // res.redirect('home')
+    }
+  })
 
-  if (!username) {
-    res.sendStatus(400)
-  }
 
 
 });
+let checkUsername = (username) =>
+  knex('users').where('name', username)
+
+let insertIntoUsers = (username) => knex('users').insert({
+  name: username
+})
 
 module.exports = router;
