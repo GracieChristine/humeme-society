3baca75bee22d8418fceac36a5feef436041658e
matthewalbrecht <MatthewWilliamAlbrecht@gmail.com>
2017-03-27 11:46:59 -0600

    "added knex files"

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+const knex = require('../knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', {
     title: 'Humeme Society'
   });
+});
+
+router.post('/', function(req, res, next) {
+  let username = req.body.username
+
+  if (!username) {
+    res.sendStatus(400)
+  }
+
+
 });
 
 module.exports = router;
